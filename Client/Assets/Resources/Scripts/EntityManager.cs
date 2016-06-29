1ae6014f8e4a6743eda9d533fbc3163f2cffa3dd
himrtaek <fs1020@naver.com>
2016-06-29 23:56:06 +0900

    코드 추가

---
+++ Client/Assets/Resources/Scripts/EntityManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EntityManager : MonoBehaviour
+{
+    Dictionary<int, Entity> _entity_list = new Dictionary<int, Entity>();
+
+    int _last_entity_id = -1;
+
+    private static EntityManager _instance;
+    public static EntityManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<EntityManager>();
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject();
+                    container.name = "EntityManager";
+                    _instance = container.AddComponent(typeof(EntityManager)) as EntityManager;
+                }
+            }
+
+
+
+            return _instance;
+        }
+    }
+
+    public Entity GetEntity(int entityID)
+    {
+        Entity entity = null;
+        if(_entity_list.ContainsKey(entityID))
+        {
+            entity = _entity_list[entityID];
+        }
+
+        return entity;
+    }
+    
+    public void AddEntity(Entity entity)
+    {
+        _entity_list.Add(++_last_entity_id, entity);
+    }
+}
