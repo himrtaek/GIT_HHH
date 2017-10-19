f49f398cc6b855daab6dfd640c10ca3692b34faf
Unknown <fs1020@naver.com>
2017-10-19 19:25:07 +0900

    SVN <-> Git Switch

---
+++ Client/Assets/Scripts/Global/Ad/AdManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.Advertisements;
+using UnityEngine;
+
+public class AdManager : SingleTone<AdManager>
+{
+	public string gameId = "1375194";
+	public bool enableTestMode = true;
+
+    public IEnumerator ShowAd()
+    {
+		if (Advertisement.isSupported)
+		{
+			Advertisement.Initialize(gameId, enableTestMode);
+		}
+
+		while (!Advertisement.isInitialized || !Advertisement.IsReady())
+		{
+			yield return new WaitForSeconds(0.5f);
+		}
+
+		Advertisement.Show();
+    }
+}
---
+++ Client/Assets/Scripts/Global/Build/Editor/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class BuildManager {
+
+	static void SetKeyStore ()
+    {
+		PlayerSettings.Android.keystoreName = "KeyStore.keystore";
+		PlayerSettings.Android.keystorePass = "rkatjd88";
+        PlayerSettings.Android.keyaliasName = "hhh";
+        PlayerSettings.Android.keyaliasPass = "rkatjd88";
+    }
+
+	[MenuItem("Build/Build Android")]
+	static void BuildAndroid()
+	{
+        SetKeyStore();
+
+		BuildPlayerOptions bpo = new BuildPlayerOptions ();
+        bpo.scenes = new string[] { "Assets/Scenes/Splash.unity" };
+		bpo.options = BuildOptions.Development | BuildOptions.AllowDebugging;
+		bpo.locationPathName = "Build/HHH.apk";
+		bpo.target = BuildTarget.Android;
+
+		BuildPipeline.BuildPlayer(bpo);
+	}
+}
