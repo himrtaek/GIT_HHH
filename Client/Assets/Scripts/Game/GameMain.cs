792450e5789c223ef8d4c9bd13014d9b053ce9cd
Unknown <fs1020@naver.com>
2017-11-02 20:40:14 +0900

    화면 핀 고정

--- Client/Assets/Scripts/Game/GameMain.cs
+++ Client/Assets/Scripts/Game/GameMain.cs
@@ -6,7 +6,8 @@ public class GameMain : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine(AdManager.Instance.ShowAd());
+        //StartCoroutine(AdManager.Instance.ShowAd());
+        Screen.SetResolution(Screen.width, Screen.width * 1080 / 1920, true);
 	}
 	
 	// Update is called once per frame
