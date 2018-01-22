c74a519ab8adf8b8528f20a69b453069f9865472
Unknown <fs1020@naver.com>
2018-01-22 21:00:27 +0900

    위로 올라가는 카메라, 사이드 벽 구현

---
+++ Trunk_HHH/Client/Assets/Scripts/Game/FollowCam.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform[] Sides;
+    public GameObject player;
+    private float fStartYPos;
+
+    private void Start()
+    {
+        fStartYPos = transform.position.y;
+    }
+
+    void LateUpdate()
+    {   
+        float fDistance = transform.position.y - player.transform.position.y;
+        if (Mathf.Abs(fDistance) > 30)
+        {
+            
+            if (fDistance >= 0)
+            {
+                if (transform.position.y < fStartYPos)
+                {
+                    return;
+                }
+                Move(Vector3.down * Mathf.Abs(fDistance) * 0.02f);
+            }
+            else
+            {
+                Move(Vector3.up * Mathf.Abs(fDistance) * 0.02f);
+            }
+        }
+    }
+
+    void Move(Vector3 moveVector)
+    {
+        transform.Translate(moveVector);
+
+        foreach(var trn in Sides)
+        {
+            trn.Translate(moveVector);
+        }
+    }
+}
